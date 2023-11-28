c14380e34358204d0ae700de4f601e5d25a0c505
ravneetkangg <ravneetkang2003@gmail.com>
2023-11-28 23:20:07 +0530

    files added

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import WeatherApp from './weather/WeatherApp';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        WeatherApp / >
+        <
+        /div>
+    );
+}
+
+export default App;
